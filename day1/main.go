d573ed38ce3a320abe35c29c20528623c8ccbc24
Mateo <mateo.ferrari97@gmail.com>
2021-12-17 17:16:20 -0300

    Add solution for part two

--- day1/main.go
+++ day1/main.go
@@ -9,21 +9,13 @@ import (
 )
 
 func main() {
-	var increments int
-
 	depths, err := getInput()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	length := len(depths)
-	for i := 1; i < length; i++ {
-		if depths[i] > depths[i-1] {
-			increments++
-		}
-	}
-
-	fmt.Println(increments)
+	solution := solvePartTwo(depths)
+	fmt.Println(solution)
 }
 
 func getInput() ([]int, error) {
@@ -45,4 +37,41 @@ func getInput() ([]int, error) {
 	}
 
 	return depths, nil
+}
+
+func solvePartOne(depths []int) int {
+	length := len(depths)
+	if length < 1 {
+		return 0
+	}
+
+	var increments int
+	for i := 1; i < length; i++ {
+		if depths[i] > depths[i-1] {
+			increments++
+		}
+	}
+
+	return increments
+}
+
+func solvePartTwo(depths []int) int {
+	length := len(depths)
+	if length < 3 {
+		return 0
+	}
+
+	prevMeasurement := depths[0] + depths[1] + depths[2]
+
+	var increments int
+	for i := 1; i+2 < length; i++ {
+		currentMeasurement := depths[i] + depths[i+1] + depths[i+2]
+		if currentMeasurement > prevMeasurement {
+			increments++
+		}
+
+		prevMeasurement = currentMeasurement
+	}
+
+	return increments
 }
